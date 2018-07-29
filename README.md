f80229776241d631cdef972b1e85ec8471e77fbe
tinoji <pot5.sunen.on.the.earth.614@gmail.com>
2018-07-30 01:44:32 +0900

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # galaxy-install-extended
 ansible-galaxy wrapper to import latest version role
+
+In development...
---
+++ main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	// flags "github.com/jessevdk/go-flags"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type Role struct {
+	Src     string `yaml:"src"`
+	Version string `yaml:"version"`
+}
+
+type Release struct {
+	TagNmae string `json:"tag_name"`
+}
+
+func main() {
+	// args
+	// TODO: Github Enterprise API endpoint and proxy option
+	if os.Args[1] == "-h" || os.Args[1] == "--help" {
+		fmt.Println("Usage: galaxy-install-extended -r FILE [options]\n\n" +
+			"Options:\n" +
+			"  -h, --help	show this help message and exit\n" +
+			"  -r ROLE_FILE	A file containing a list of roles to be imported\n" +
+			"  See 'ansible-galaxy install --help' for other options")
+
+		os.Exit(0)
+	}
+
+	if os.Args[1] != "-r" {
+		log.Fatal("First option must be -r ROLE_FILE")
+	}
+
+	reqFile := os.Args[2]
+	otherOpts := strings.Join(os.Args[3:len(os.Args)], " ")
+
+	// read yaml and conv "lastet" to latest release
+	buf, err := ioutil.ReadFile(reqFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var roles []Role
+	err = yaml.Unmarshal(buf, &roles)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i, role := range roles {
+		if role.Version == "latest" {
+			repo := getRepoName(role.Src)
+			tag := getTagName(repo)
+			roles[i].Version = tag
+		}
+	}
+
+	d, err := yaml.Marshal(&roles)
+
+	// make tmp file
+	tmpFile, _ := ioutil.TempFile("", "tmp")
+	tmpFile.Write(([]byte)(d))
+
+	tmpYaml := tmpFile.Name() + ".yml"
+	err = os.Rename(tmpFile.Name(), tmpYaml)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer os.Remove(tmpYaml)
+
+	// exec
+	command := "ansible-galaxy install -r " + tmpYaml + " " + otherOpts
+	out, err := exec.Command("sh", "-c", command).Output()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println(string(out))
+}
+
+func getRepoName(src string) string {
+	s := strings.Split(src, "/")
+	len := len(s)
+	return s[len-2] + "/" + s[len-1]
+}
+
+func getTagName(repo string) string {
+	url := "https://api.github.com/repos/" + repo + "/releases/latest"
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var d Release
+	err = json.Unmarshal(body, &d)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return d.TagNmae
+}
